05a45d5553486f27c4b7b307996f14ecf67b22e9
Chris Palmer <capalmer1013@gmail.com>
2022-12-22 01:02:51 -0500

    some inheritance to combine menu functionality for compare and home page views

--- neuralut/app.py
+++ neuralut/app.py
@@ -34,10 +34,10 @@ class App(tk.Tk):
         ## Initialize Frames
         self.frames = {}
         self.HomePage = HomePage
-        self.Validation = Validation
+        self.Compare = Compare
 
         ## Defining Frames and Packing it
-        for F in {HomePage, Validation}:
+        for F in {HomePage, Compare}:
             frame = F(self, container)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")    
@@ -52,6 +52,36 @@ class App(tk.Tk):
  
 
 
+
+class View(tk.Frame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+    
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Import", command=lambda: self.fileList.browseFiles())
+
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
+
+        ## View Menu
+        viewmenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="View", menu=viewmenu)
+        viewmenu.add_command(label="Home", command=lambda: parent.show_frame(parent.HomePage))
+        viewmenu.add_command(label="Compare", command=lambda: parent.show_frame(parent.Compare))
+
+
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command=U.about)
+        help_menu.add_separator()
+
+        return menubar
 
 #---------------------------------------- HOME PAGE FRAME / CONTAINER ------------------------------------------------------------------------
 class FileList(tk.Frame):
@@ -131,9 +161,9 @@ class PreviewWindow(tk.Frame):
         self.image_id = self.canvas.create_image(0, 0, anchor=NW, image=self.img)
 
 
-class HomePage(tk.Frame):
+class HomePage(View):
     def __init__(self, parent, container):
-        super().__init__(container)
+        super().__init__(self, container)
 
         label = tk.Label(self, text="Neuralut", font=('Times', '20'))
         label.grid()
@@ -143,60 +173,17 @@ class HomePage(tk.Frame):
         self.fileList = FileList(self, self, preview=self.preview)
         self.fileList.grid(column=0, row=0)
 
-    def create_menubar(self, parent):
-        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
-
-        ## Filemenu
-        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
-        menubar.add_cascade(label="File", menu=filemenu)
-        filemenu.add_command(label="Import", command=lambda: self.fileList.browseFiles())
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=parent.quit)  
-
-        ## help menu
-        help_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=U.about)
-        help_menu.add_separator()
-
-        return menubar
-
 
 #---------------------------------------- Validation PAGE FRAME / CONTAINER ------------------------------------------------------------------------
 
-class Validation(tk.Frame):
+class Compare(View):
     def __init__(self, parent, container):
-        super().__init__(container)
+        super().__init__(self, container)
 
         label = tk.Label(self, text="Validation Page", font=('Times', '20'))
         label.pack(pady=0,padx=0)
 
         ## ADD CODE HERE TO DESIGN THIS PAGE
-
-    def create_menubar(self, parent):
-        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
-
-        ## Filemenu
-        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
-        menubar.add_cascade(label="File", menu=filemenu)
-        filemenu.add_command(label="New Project", command=lambda: parent.show_frame(parent.Validation))
-        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=parent.quit)  
-
-        ## proccessing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
-
-        ## help menu
-        help_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=U.about)
-        help_menu.add_separator()
-
-        return menubar
 
 
 if __name__ == "__main__":
