f366bcb4f9de74a7d03b9e917eb3917f0522400a
Chris Palmer <capalmer1013@gmail.com>
2023-02-15 22:26:29 -0500

    some noise reduction stuff

--- neuralut/rawtest.py
+++ neuralut/rawtest.py
@@ -7,6 +7,9 @@ with rawpy.imread(path) as raw:
         use_camera_wb=True,
         exp_shift=3.0,
         #highlight_mode=rawpy.HighlightMode.Blend,
+        # fbdd_noise_reduction=rawpy.FBDDNoiseReductionMode.Full,
+        # noise_thr=10.0,
+
     )
     rgb = raw.postprocess(params=params)
 imageio.imsave('cr3-to.tiff', rgb)
