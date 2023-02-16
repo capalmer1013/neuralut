634cf4e914953dee0ba9f01fe8f6fbecc4086f5c
Chris Palmer <capalmer1013@gmail.com>
2023-02-15 21:26:44 -0500

    added postprocess params for rawpy

--- neuralut/rawtest.py
+++ neuralut/rawtest.py
@@ -3,5 +3,10 @@ import imageio
 
 path = 'tests/assets/600A9641.CR3'
 with rawpy.imread(path) as raw:
-    rgb = raw.postprocess()
+    params = rawpy.Params(
+        use_camera_wb=True,
+        exp_shift=3.0,
+        #highlight_mode=rawpy.HighlightMode.Blend,
+    )
+    rgb = raw.postprocess(params=params)
 imageio.imsave('cr3-to.tiff', rgb)
